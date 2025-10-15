a61815509222f99aa53cfc58825759f9e2b71616
jacobstouffer <jacobstouffer215@gmail.com>
2025-10-15 11:04:34 -0700

    get next verse + unit tests

--- src/tests/reference.test.js
+++ src/tests/reference.test.js
@@ -1,5 +1,55 @@
 import { expect, test, describe } from 'vitest';
-import { getPrevious } from '../utils/reference';
+import { getPrevious, getNext } from '../utils/reference';
+
+describe('getNext', () => {
+  test('Return next from same chapter', () => {
+    const verseMap = new Map();
+    verseMap.set('10', [1, 2, 3, 4, 5]);
+    const values = getNext(2, 10, verseMap);
+
+    expect(values.verse).toEqual(3);
+  });
+  
+  test('Return next from different chapter', () => {
+    const verseMap = new Map();
+    verseMap.set('10', [1, 2, 3, 4, 5]);
+    verseMap.set('11', [1, 2, 3, 4, 5]);
+    const values = getNext(5, 10, verseMap);
+
+    expect(values.verse).toEqual(1);
+  });
+
+  test('Return next when verse is first in chapter', () => {
+    const verseMap = new Map();
+    verseMap.set('10', [1, 2, 3, 4, 5]);
+    verseMap.set('11', [1, 2, 3, 4, 5]);
+    const values = getNext(1, 10, verseMap);
+
+    expect(values.verse).toEqual(2);
+  });
+
+  test('When there is no next chapter', () => {
+    const verseMap = new Map();
+    verseMap.set('10', [1, 2, 3, 4, 5]);
+    verseMap.set('11', [1, 2, 3, 4, 5]);
+    const values = getNext(5, 11, verseMap);
+
+    expect(values.verse).toEqual(0);
+  });
+
+  test('When null passed in', () => {
+    const values = getNext(10, 2, null);
+    expect(values.verse).toEqual(0);
+  });
+
+  test('When current verse is not in map', () => {
+    const verseMap = new Map();
+    verseMap.set('10', [1, 3, 4, 5]);
+    const values = getNext(10, 2, verseMap);
+    expect(values.verse).toEqual(0);
+  });
+
+});
 
 describe('getPrevious', () => {
   test('Return previous from same chapter', () => {
@@ -44,6 +94,15 @@ describe('getPrevious', () => {
     const values = getPrevious(2, 11, verseMap);
 
     expect(values.verse).toEqual(5);
+  });
+  
+  test('When current verse is last in the chapter', () => {
+    const verseMap = new Map();
+    verseMap.set('10', [1, 2, 3, 4, 5]);
+    verseMap.set('11', [2, 3, 4, 5]);
+    const values = getPrevious(5, 11, verseMap);
+
+    expect(values.verse).toEqual(4);
   });
   
   test('When null passed in', () => {
--- src/utils/reference.ts
+++ src/utils/reference.ts
@@ -3,53 +3,94 @@ function refObj(c: number, v: number) {
 }
 
 export function getPrevious(
-    currentVerse: number | undefined,
-    currentChapter: number | undefined, 
-    verseMap: Map<string, number[]> | null
-  ): { chapter: number, verse: number } {
-    if (!verseMap || !currentVerse || !currentChapter) {
+  currentVerse: number | undefined,
+  currentChapter: number | undefined,
+  verseMap: Map<string, number[]> | null
+): { chapter: number; verse: number } {
+  if (!verseMap || !currentVerse || !currentChapter) {
+    return refObj(0, 0);
+  }
+
+  try {
+    // Most of the time, the previous verse is within the same chapter, i.e 10:5, previous is 10:4
+    let prevChapter = currentChapter;
+    let previousVerses = verseMap.get(currentChapter.toString());
+
+    // If this is verse 1 OR if the first verse in the chapter is this verse (rare case, not anticipated)
+    if (currentVerse === 1 || (previousVerses && previousVerses[0] === currentVerse)) {
+      prevChapter = currentChapter - 1;
+      previousVerses = verseMap.get(prevChapter.toString());
+      if (previousVerses && previousVerses.length > 0) {
+        // Get the last verse in the chapter
+        return refObj(prevChapter, previousVerses[previousVerses.length - 1]);
+      } else {
+        return refObj(0, 0);
+      }
+    }
+
+    // If there is only one verse in the chapter and it's not verse 1 (rare case, not anticipated)
+    if (!previousVerses || previousVerses.length < 2) {
       return refObj(0, 0);
     }
 
-    try {
-      let prevVerse;
-      // Most of the time, the previous verse is within the same chapter, i.e 10:5, previous is 10:4
-      let prevChapter = currentChapter; 
-      let previousVerses = verseMap.get(currentChapter.toString());
+    // Find the current verse and select the one prior to it in the list
+    for (let i = 1; i < previousVerses.length; i++) {
+      if (previousVerses[i] === currentVerse) {
+        return refObj(prevChapter, previousVerses[i - 1]);
+      } else if (previousVerses[i - 1] >= currentVerse) {
+        return refObj(0, 0);
+      }
+    }
 
-      // If this is verse 1 OR if the first verse in the chapter is this verse (rare case, not anticipated)
-      if (currentVerse === 1 || (previousVerses && previousVerses[0] === currentVerse)) {
-        prevChapter = currentChapter - 1;
-        previousVerses = verseMap.get(prevChapter.toString());
-        if (previousVerses && previousVerses.length > 0) {
-          // Get the last verse in the chapter
-          prevVerse = previousVerses[previousVerses.length - 1];
-        } else {
-          return refObj(0, 0);
-        }
+    return refObj(0, 0);
+  } catch (error) {
+    console.error('Error in getPrevious:', error);
+    return refObj(0, 0);
+  }
+}
 
-        return refObj(prevChapter, prevVerse);
-      } 
+export function getNext(
+  currentVerse: number | undefined,
+  currentChapter: number | undefined,
+  verseMap: Map<string, number[]> | null
+): { chapter: number; verse: number } { 
+  if (!verseMap || !currentVerse || !currentChapter) {
+    return refObj(0, 0);
+  }
 
-      // If there is only one verse in the chapter and it's not verse 1 (rare case, not anticipated)
-      if (!previousVerses || previousVerses.length < 2) {
+  try {
+    // Most of the time, the next verse is within the same chapter, i.e. 10:14, next is 10:15
+    let nextChapter = currentChapter;
+    let nextVerses = verseMap.get(currentChapter.toString());
+    
+    if (!nextVerses || nextVerses.length === 0) {
+      console.error("There are no verses in the current chapter", currentChapter);
+      return refObj(0, 0);
+    }
+
+    // If the last verse of the chapter is the current verse, go to the next chapter
+    if (nextVerses[nextVerses.length - 1] === currentVerse) {
+      nextChapter = currentChapter + 1;
+      nextVerses = verseMap.get(nextChapter.toString());
+      if (!nextVerses || nextVerses.length === 0) {
         return refObj(0, 0);
       }
 
-      // Find the current verse and select the one prior to it in the list
-      for (let i = 1; i <= previousVerses.length; i++) {
-        if (previousVerses[i] === currentVerse) {
-          return refObj(prevChapter, previousVerses[i - 1]);
-        } else if (previousVerses[i - 1] >= currentVerse) {
-          return refObj(0, 0);
-        }
-
-        i++;
-      }
-
-      return refObj(0, 0);
-    } catch (error) {
-      console.error("Error in getPrevious:", error);
-      return refObj(0, 0);
+      return refObj(nextChapter, nextVerses[0]);
     }
+
+    // Find the current verse and select the one after it in the list
+    for (let i = 0; i < nextVerses.length - 2; i++) {
+      if (nextVerses[i] === currentVerse) {
+        return refObj(nextChapter, nextVerses[i + 1]);
+      } else if (nextVerses[i + 1] < currentVerse) {
+        return refObj(0, 0);
+      }
+    }
+    return refObj(0, 0);
+  } catch (error) {
+    console.error('Error in getNext:', error);
+    return refObj(0, 0);
   }
+}
+
