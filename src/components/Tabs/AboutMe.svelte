520835b3b588b37d6ea41514af7c2e15fa965b31
jacobstouffer <jacobstouffer215@gmail.com>
2025-11-20 16:47:05 -0800

    Update about tabs

--- src/components/Tabs/AboutMe.svelte
+++ src/components/Tabs/AboutMe.svelte
@@ -1,81 +1,63 @@
 <div>
     <h3>
-        <span class="TextRun SCXW148780909 BCX0 NormalTextRun">The author</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+        The author
     </h3>
 </div>
 <div>
     <p>
-        <span class="TextRun SCXW148780909 BCX0 NormalTextRun">My name is Jacob Stouffer. I live in California with my wonderful wife, Mariana. We are members of Grace Community Church. I became a Christian at age 30. Before then, I was what Solomon would call a fool. This means that I had 30 years of experience with foolish thinking, speaking, looking, and walking. I was saturated in habits of foolishness. But God, in his kindness, gave me a new life. The book of Proverbs has been instrumental in bringing me out of foolish and wicked habits and helping me walk according to God’s right path.&nbsp;</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+        My name is Jacob Stouffer. I live in California with my wonderful wife, Mariana. We are members of Grace Community Church. I became a Christian at age 30. Before then, I was what Solomon would call a fool. This means that I had 30 years of experience with foolish thinking, speaking, looking, and walking. I was saturated in habits of foolishness. But God, in his kindness, gave me a new life. The book of Proverbs has been instrumental in bringing me out of foolish and wicked habits and helping me walk according to God’s right path.
     </p>
 </div>
 <div>
     <p>
-        <span class="TextRun SCXW148780909 BCX0 NormalTextRun">I have been and continue to be instructed, encouraged, and challenged by the Proverbs. And I love using the Proverbs to instruct, encourage, and challenge others. To that end, I have set out to write a devotional for each of the proverbs from chapter 10 through chapter 29. In writing these devotionals, I endeavor to present the truth of God’s Word with accuracy, clarity, and conviction. I endeavor to teach the truth of God’s Word only after it has challenged and impacted my own heart (Colossians 3:16).</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+        I have been and continue to be instructed, encouraged, and challenged by the Proverbs. And I love using the Proverbs to instruct, encourage, and challenge others. To that end, I have set out to write a devotional for each of the proverbs from chapters 10 through 29. In writing these devotionals, I endeavor to present the truth of God’s Word with accuracy, clarity, and conviction. I endeavor to teach the truth of God’s Word only after it has challenged and impacted my own heart (Colossians 3:16).
     </p>
 </div>
 <div>
     <h3>
-        <span class="TextRun SCXW148780909 BCX0 NormalTextRun">Adoption Fund</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+        Adoption Fund
     </h3>
 </div>
 <div>
     <p>
-        <span class="TextRun SCXW148780909 BCX0 NormalTextRun">My wife and I are applying for international adoption from Columbia! A big reason that I am writing these devotionals is that I want to be able to share the wisdom from God’s Word with my future children. If you have been blessed by this project and would like to make a one-time contribution to our adoption fund, my wife and I would be greatly blessed. All donations will be used for adoption related expenses. The adoption process will likely take 1-2 more years. I will update this page with any major developments in the adoption process as they happen.</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+        My wife and I are applying for international adoption from Columbia! A big reason that I am writing these devotionals is that I want to be able to share the wisdom from God’s Word with my future children. If you have been blessed by this project and would like to make a one-time contribution to our adoption fund, my wife and I would be greatly blessed. All donations will be used for adoption related expenses. The adoption process will likely take 1-2 more years. I will update this page with any major developments in the adoption process as they happen.
     </p>
     <p><a href="/about?tab=about-me">[Link here when available]</a></p>
 </div>
 <div>
     <h3>
-        <span class="TextRun SCXW148780909 BCX0 NormalTextRun">My beliefs</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+        My beliefs
     </h3>
 </div>
 <div>
     <p>
-        <span class="TextRun SCXW148780909 BCX0 NormalTextRun">I am an evangelical Christian. I agree with the Statement of Faith of my church. In these devotionals, the authority and inerrancy of Scripture are a non-negotiable starting point. The uniqueness of Jesus as the only path to salvation is a given.&nbsp;</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+        I am an evangelical Christian. I agree with the Statement of Faith of my church. In these devotionals, the authority and inerrancy of Scripture are a non-negotiable starting point. The uniqueness of Jesus as the only path to salvation is a given.
     </p>
 </div>
 <div>
     <p>
-        <span class="TextRun SCXW148780909 BCX0 NormalTextRun">Many secondary doctrines, however, are less relevant to the proverbs. For example, here are some topics that will probably not arise in these devotionals:&nbsp;</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+        Many secondary doctrines, however, are less relevant to the proverbs. For example, here are some topics that will probably not arise in these devotionals:
     </p>
-</div>
-<div>
     <ul>
         <li>
-            <span class="TextRun SCXW148780909 BCX0 NormalTextRun">church governance</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+            church governance
         </li>
-    </ul>
-</div>
-<div>
-    <ul>
         <li>
-            <span class="TextRun SCXW148780909 BCX0 NormalTextRun">mode of baptism</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+            mode of baptism
         </li>
-    </ul>
-</div>
-<div>
-    <ul>
         <li>
-            <span class="TextRun SCXW148780909 BCX0 NormalTextRun">dispensationalism and covenant theology</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+            dispensationalism and covenant theology
         </li>
-    </ul>
-</div>
-<div>
-    <ul>
         <li>
-            <span class="TextRun SCXW148780909 BCX0 NormalTextRun">the cessation or continuation of spiritual gifts</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+            the cessation or continuation of spiritual gifts
         </li>
-    </ul>
-</div>
-<div>
-    <ul>
         <li>
-            <span class="TextRun SCXW148780909 BCX0 NormalTextRun">the millennium.&nbsp;</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+            the millennium
         </li>
     </ul>
 </div>
 <div>
     <p>
-        <span class="TextRun SCXW148780909 BCX0 NormalTextRun">I pray that these devotionals will be a blessing to my beloved brothers and sisters in Christ who hold various beliefs about secondary and tertiary doctrines.</span><span class="EOP SCXW148780909 BCX0">&nbsp;</span>
+        I pray that these devotionals will be a blessing to my beloved brothers and sisters in Christ who hold various beliefs about secondary and tertiary doctrines.
     </p>
 </div>
